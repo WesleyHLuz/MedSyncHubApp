691a42b1a2e924cc7eb290765cb90c583954cd74
Wesley <wesdias5@outlook.com>
2025-02-23 18:17:03 -0300

    Wesley 23/02/25

---
+++ MedSyncHub/pages/login/index.tsx
@@ -0,0 +1,46 @@
+﻿import React from "react";
+import {Image, Text, TextInput, TouchableOpacity, View} from "react-native";
+import {styles} from "./styles";
+
+import {MaterialIcons} from "@expo/vector-icons";
+
+// @ts-ignore
+import logo from "../../assets/logo.jpg";
+import {themes} from "../../global/themes";
+
+export default function Login() {
+    return (
+        <View style={styles.container}>
+            <View style={styles.boxTop}>
+                <Image source={logo}
+                style={styles.logo}
+                resizeMode="contain"/>
+                <Text style={styles.text}>Bem vindo de volta!</Text>
+            </View>
+            <View style={styles.boxMid}>
+                <Text style={styles.titleInput}>Endereço de email</Text>
+                <View style={styles.boxInput}>
+                    <TextInput style={styles.input}/>
+                    <MaterialIcons
+                    name="email"
+                    size={20}
+                    color={themes.colors.tertiary}/>
+                </View>
+                <Text style={styles.titleInput}>SENHA</Text>
+                <View style={styles.boxInput}>
+                    <TextInput style={styles.input}/>
+                    <MaterialIcons
+                        name="remove-red-eye"
+                        size={20}
+                        color={themes.colors.tertiary}/>
+                </View>
+            </View>
+            <View style={styles.boxBottom}>
+                <TouchableOpacity style={styles.button}>
+                    <Text style={styles.textButton}>Entrar</Text>
+                </TouchableOpacity>
+            </View>
+
+        </View>
+    )
+}
---
+++ MedSyncHub/pages/login/styles.ts
@@ -0,0 +1,78 @@
+﻿import {Dimensions, StyleSheet} from "react-native";
+import {themes} from "../../global/themes";
+
+export const styles = StyleSheet.create(
+    {
+        container: {
+            flex: 1,
+            alignItems: 'center',
+            justifyContent: 'center',
+        },
+        boxTop: {
+            height:Dimensions.get("window").height/3,
+            width: '100%',
+            backgroundColor: 'white',
+            alignItems: 'center',
+
+        },
+        boxMid: {
+            height:Dimensions.get("window").height/4,
+            width: '100%',
+            backgroundColor: 'white',
+            paddingHorizontal: 37
+        },
+        boxBottom: {
+            height:Dimensions.get("window").height/3,
+            width: '100%',
+            backgroundColor: 'white',
+            alignItems: 'center',
+            //justifyContent: 'center',
+        },
+        logo: {
+            width: 150,
+            height: 200,
+            marginHorizontal: 120
+        },
+        text: {
+            fontWeight: 'bold',
+            fontSize: 18,
+            marginTop: 40
+        },
+        titleInput: {
+            marginLeft: 5,
+            marginTop: 20,
+            color:themes.colors.secondaryText
+        },
+        boxInput: {
+            width: '100%',
+            height: 40,
+            borderWidth: 1,
+            borderRadius: 40,
+            marginTop: 10,
+            flexDirection: 'row',
+            alignItems: 'center',
+            paddingHorizontal: 10,
+            backgroundColor: 'white',
+            borderColor: themes.colors.tertiary,
+        },
+        input: {
+            height: '100%',
+            width: '90%'
+        },
+        button: {
+            width: 200,
+            height: 50,
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: themes.colors.primary,
+            borderRadius: 40,
+        },
+        textButton: {
+            fontSize: 14,
+            color: themes.colors.text,
+            fontWeight: 'bold',
+
+        }
+
+    }
+)
